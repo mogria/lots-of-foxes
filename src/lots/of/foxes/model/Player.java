df1c1df180fa79aa289eb18b7198dbd6471debcc
Moritz Küttel <moritz.kuettel@hslu.ch>
2015-12-03 10:06:15 +0100

    implement Player model

--- src/lots/of/foxes/model/Player.java
+++ src/lots/of/foxes/model/Player.java
@@ -1,9 +1,63 @@
 package lots.of.foxes.model;
 
+import java.awt.Color;
+
 /**
  *
  * @author Moritz
  */
 public class Player {
+       
+    /**
+     * name of the player
+     */
+    private String name;
     
+    /**
+     * the color of this player
+     */
+    private Color color;
+    
+    /**
+     * the number of boxes owned by this player
+     */
+    private int boxCount = 0;
+
+    public Player(String name, Color color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    /**
+     * Get the name of this player
+     *
+     * @return the name of this player
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Get the color of this player
+     * @return the color of this player
+     */
+    public Color getColor() {
+        return color;
+    }
+    
+    /**
+     * Get the number of boxes owned by this player
+     *
+     * @returnthe number of boxes owned by this player
+     */
+    public int getBoxCount() {
+        return boxCount;
+    }
+
+    /**
+     * increases boxCount by 1
+     */
+    public void increaseBoxCount() {
+        boxCount++;
+    } 
 }
