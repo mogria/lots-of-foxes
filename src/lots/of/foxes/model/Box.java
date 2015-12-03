226a4d011b31cf730528f42003af545d765256f9
Moritz Küttel <moritz.kuettel@hslu.ch>
2015-12-03 09:53:24 +0100

    implement Box class, create empty Player class

---
+++ src/lots/of/foxes/model/Box.java
@@ -0,0 +1,52 @@
+package lots.of.foxes.model;
+
+/**
+ * Represents a single box on the Board
+ * 
+ * @author Moritz
+ */
+public class Box {
+    
+    /**
+     * the number of lines used around this box
+     */
+    private int lineCount = 0;
+    
+    /**
+     * the player which used the last line.
+     */
+    private Player owner;
+
+    /**
+     * Get the value of lineCount
+     *
+     * @return the value of lineCount
+     */
+    public int getLineCount() {
+        return lineCount;
+    }
+
+    /**
+     * Get the value of owner
+     *
+     * @return the value of owner
+     */
+    public Player getOwner() {
+        return owner;
+    }
+    
+    /**
+     * Increases the number of lines in use around this Box.
+     * if the fourth line is being taken the player passed is declared owner
+     * of the Box.
+     * 
+     * @param player the player adding the line
+     */
+    public void addLine(Player player) {
+        lineCount++;
+        if(lineCount >= 4) {
+            owner = player;
+        }
+            
+    }
+}
