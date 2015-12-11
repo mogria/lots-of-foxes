d04baba414332004e719269065a4ea02a2e6426b
Moritz Küttel <moritz.kuettel@hslu.ch>
2015-12-11 16:58:20 +0100

    prioritize boxes which have less than 3 lines set

--- src/lots/of/foxes/ai/NotSoDumbAITurnHandler.java
+++ src/lots/of/foxes/ai/NotSoDumbAITurnHandler.java
@@ -1,5 +1,6 @@
 package lots.of.foxes.ai;
 
+import java.util.ArrayList;
 import java.util.List;
 import lots.of.foxes.model.Board;
 import lots.of.foxes.model.Box;
@@ -20,11 +21,19 @@ public class NotSoDumbAITurnHandler extends AbstractAITurnHandler {
     public Line receiveTurn() {
         List<Box> closableBoxes = BoardUtil.getCloseableBoxes(board);
         if(closableBoxes.size() > 0) {
-            List<Line> closingLines = BoardUtil.getLinesSurroundingBox(board, closableBoxes.get(0));
+            List<Line> closingLines = BoardUtil.filterAvailableLines(BoardUtil.getLinesSurroundingBox(board, closableBoxes.get(0)));
             if(closingLines.size() > 0) {
                 return closingLines.get(0);
             }
+
+            
         }
+                    
+        List<Box> suboptimalBoxes = BoardUtil.filterBoxes(new ArrayList<>(board.getBoxes()), box -> box.getLineCount() < 3);
+        if(suboptimalBoxes.size() > 0) {
+            return BoardUtil.getRandomFromList(BoardUtil.filterAvailableLines(BoardUtil.getLinesSurroundingBox(board, BoardUtil.getRandomFromList(suboptimalBoxes))));
+        }
+            
         return BoardUtil.getRandomAvailableLine(board);
     }
     
