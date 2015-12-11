8e1d2a3d9cb819a3c0d238952af3e54352a9bde3
nortismo <diego.bienz@gmail.com>
2015-12-11 16:08:04 +0100

    Game Info

--- src/lots/of/foxes/ui/game/UITurnHandler.java
+++ src/lots/of/foxes/ui/game/UITurnHandler.java
@@ -1,5 +1,6 @@
 package lots.of.foxes.ui.game;
 
+import java.awt.BorderLayout;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
@@ -29,8 +30,9 @@ public class UITurnHandler extends AbstractTurnHandler {
         this.parentThread = parentThread;
         boardUI = new BoardUI(board, 10, 50, turnLock);
         gameInfo = new GameInfo();
-        frame.add(boardUI);
-        frame.add(gameInfo);
+        frame.setLayout(new BorderLayout());
+        frame.add(boardUI, BorderLayout.WEST);
+        frame.add(gameInfo, BorderLayout.EAST);
         uiThread = new Thread(boardUI);
         uiThread.start();
     }
